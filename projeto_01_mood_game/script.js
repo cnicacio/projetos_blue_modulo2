c2a79ca914487215a4398bc029a4f757361f864e
Carlos Nicacio <carlosnicacio94@gmail.com>
2021-07-19 17:29:25 -0300

    Projeto 01 - Módulo 02 - Blue EdTech

--- projeto_01_mood_game/script.js
+++ projeto_01_mood_game/script.js
@@ -1,28 +1,29 @@
 let imagem = document.getElementById('michael')
 let botao = document.getElementById('mudar_humor')
+let texto = document.getElementById('michael1')
 
 botao.onclick = function(){
     if (botao.value === "confiante"){
         imagem.src = 'images/chateado.jpg';
         botao.value = 'chateado';
-        document.getElementById('michael1').innerHTML = "#chateado por um novo corte no orçamento vindo da sede"
+        texto.innerHTML = "#chateado por um novo corte no orçamento vindo da sede"
     }
 
     else if (botao.value === "chateado"){
         imagem.src = 'images/cagada.jpg';
         botao.value = 'cagada';
-        document.getElementById('michael1').innerHTML = "preparado pra levar uma bronca sabendo que fiz uma cagada"
+        texto.innerHTML = "preparado pra levar uma bronca sabendo que fiz uma cagada"
     }
 
     else if (botao.value === "cagada"){
         imagem.src = 'images/pistola.jpg'
         botao.value = 'pistola'
-        document.getElementById('michael1').innerHTML = "extremamente pistola e revoltado"
+        texto.innerHTML = "extremamente pistola e revoltado"
     }
 
     else if (botao.value === "pistola"){
         imagem.src = 'images/confiante.jpg'
         botao.value = 'confiante'
-        document.getElementById('michael1').innerHTML = "confiante para mais um dia de trabalho"
+        texto.innerHTML = "confiante para mais um dia de trabalho"
     }
 }
